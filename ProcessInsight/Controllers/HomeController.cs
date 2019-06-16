53fe77f17cbf252dd10739218845e4e1f80ec3f2
Paul Watson <psjw12@users.noreply.github.com>
2019-06-16 15:09:44 +0100

    Added code to print list of processes

--- ProcessInsight/Controllers/HomeController.cs
+++ ProcessInsight/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ProcessInsight.Models;
 
 namespace ProcessInsight.Controllers
 {
@@ -12,7 +11,11 @@ namespace ProcessInsight.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var processes = System.Diagnostics.Process.GetProcesses();
+
+            var processNames = processes.Select(p => p.ProcessName);
+
+            return View(processNames);
         }
     }
 }
